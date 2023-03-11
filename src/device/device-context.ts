1a7e064fcdfe9652381c4dbcb45bebc5e760972c
Joe Davis <joedavis29@gmail.com>
2023-03-11 15:32:57 -0500

    remove amplify

--- src/device/device-context.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { MetaWearState, DefaultMetaWearState } from './ios/metawear-ios';
-
-const DeviceContext = React.createContext<
-  [MetaWearState,(v: MetaWearState) => void]
->([DefaultMetaWearState, () => null]);
-
-export default DeviceContext;
